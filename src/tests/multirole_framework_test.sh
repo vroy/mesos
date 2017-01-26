5bd2eb1cd393f0eeb724cf5ffc5ea85516f90dcd
Benjamin Bannier <benjamin.bannier@mesosphere.io>
2017-01-26 09:50:57 +0100

    Slightly refactored driver script.

--- src/tests/multirole_framework_test.sh
+++ src/tests/multirole_framework_test.sh
@@ -6,88 +6,79 @@
 set -e
 set -o pipefail
 
-source ${MESOS_SOURCE_DIR}/support/colors.sh
-source ${MESOS_SOURCE_DIR}/support/atexit.sh
+function setup_env {
+  source ${MESOS_SOURCE_DIR}/support/colors.sh
+  source ${MESOS_SOURCE_DIR}/support/atexit.sh
 
-MASTER_PID=
-AGENT_PID=
-MESOS_WORK_DIR=`mktemp -d -t mesos-XXXXXX`
-MESOS_RUNTIME_DIR=`mktemp -d -t mesos-XXXXXX`
+  export LD_LIBRARY_PATH=${MESOS_BUILD_DIR}/src/.libs
+  MASTER=${MESOS_SBIN_DIR}/mesos-master
+  AGENT=${MESOS_SBIN_DIR}/mesos-agent
+  MULTIROLE_FRAMEWORK=${MESOS_HELPER_DIR}/multirole-framework
 
-
-function cleanup() {
-  # Make sure we kill the master on exit.
-  if [[ ! -z ${MASTER_PID} ]]; then
-    kill ${MASTER_PID}
-  fi
-
-  # Make sure we kill the agent on exit.
-  if [[ ! -z ${AGENT_PID} ]]; then
-    kill ${AGENT_PID}
-  fi
-
-  if [[ -d "${MESOS_WORK_DIR}" ]]; then
-    rm -rf "${MESOS_WORK_DIR}"
-  fi
-
-  if [[ -d "${MESOS_RUNTIME_DIR}" ]]; then
-    rm -rf "${MESOS_RUNTIME_DIR}"
-  fi
+  # The mesos binaries expect MESOS_ prefixed environment variables
+  # to correspond to flags, so we unset these here.
+  unset MESOS_BUILD_DIR
+  unset MESOS_SOURCE_DIR
+  unset MESOS_HELPER_DIR
+  unset MESOS_VERBOSE
 }
 
-atexit cleanup
+function start_master {
+  MESOS_WORK_DIR=$(mktemp -d -t mesos-master-XXXXXX)
+  atexit rm -rf "${MESOS_WORK_DIR}"
 
-export LD_LIBRARY_PATH=${MESOS_BUILD_DIR}/src/.libs
-MASTER=${MESOS_SBIN_DIR}/mesos-master
-AGENT=${MESOS_SBIN_DIR}/mesos-agent
-MULTIROLE_FRAMEWORK=${MESOS_HELPER_DIR}/multirole-framework
-
-# The mesos binaries expect MESOS_ prefixed environment variables
-# to correspond to flags, so we unset these here.
-unset MESOS_BUILD_DIR
-unset MESOS_SOURCE_DIR
-unset MESOS_HELPER_DIR
-unset MESOS_VERBOSE
-
-# Launch master.
-${MASTER} \
+  ${MASTER} \
     --ip=127.0.0.1 \
     --port=5432 \
     --work_dir="${MESOS_WORK_DIR}" &
-MASTER_PID=${!}
-echo "${GREEN}Launched master at ${MASTER_PID}${NORMAL}"
-# sleep 2 # FIXME(bbannier): reneable?
+  MASTER_PID=${!}
+  echo "${GREEN}Launched master at ${MASTER_PID}${NORMAL}"
 
-# Check the master is still running after 2 seconds.
-kill -0 ${MASTER_PID} >/dev/null 2>&1
-STATUS=${?}
-if [[ ${STATUS} -ne 0 ]]; then
-  echo "${RED}Master crashed; failing test${NORMAL}"
-  exit 2
-fi
+  # Check the master is still running after 2 seconds.
+  kill -0 ${MASTER_PID} >/dev/null 2>&1
+  STATUS=${?}
+  if [[ ${STATUS} -ne 0 ]]; then
+    echo "${RED}Master crashed; failing test${NORMAL}"
+    exit 2
+  fi
 
-# Disable support for systemd as this test does not run as root.
-# This flag must be set as an environment variable because the flag
-# does not exist on non-Linux builds.
-export MESOS_SYSTEMD_ENABLE_SUPPORT=false
+  atexit kill ${MASTER_PID}
+}
 
-# Launch agent.
-${AGENT} \
+function start_agent {
+  # Disable support for systemd as this test does not run as root.
+  # This flag must be set as an environment variable because the flag
+  # does not exist on non-Linux builds.
+  export MESOS_SYSTEMD_ENABLE_SUPPORT=false
+
+  MESOS_WORK_DIR=$(mktemp -d -t mesos-master-XXXXXX)
+  atexit rm -rf "${MESOS_WORK_DIR}"
+
+  MESOS_RUNTIME_DIR=`mktemp -d -t mesos-XXXXXX`
+  atexit rm -rf "${MESOS_RUNTIME_DIR}"
+
+  ${AGENT} \
     --work_dir="${MESOS_WORK_DIR}" \
     --runtime_dir="${MESOS_RUNTIME_DIR}" \
     --master=127.0.0.1:5432 \
     --resources="cpus:1;mem:96;disk:50" &
-AGENT_PID=${!}
-echo "${GREEN}Launched agent at ${AGENT_PID}${NORMAL}"
-# sleep 2 # FIXME(bbannier): reenable?
+  AGENT_PID=${!}
+  echo "${GREEN}Launched agent at ${AGENT_PID}${NORMAL}"
 
-# Check the agent is still running after 2 seconds.
-kill -0 ${AGENT_PID} >/dev/null 2>&1
-STATUS=${?}
-if [[ ${STATUS} -ne 0 ]]; then
-  echo "${RED}Slave crashed; failing test${NORMAL}"
-  exit 2
-fi
+  # Check the agent is still running after 2 seconds.
+  kill -0 ${AGENT_PID} >/dev/null 2>&1
+  STATUS=${?}
+  if [[ ${STATUS} -ne 0 ]]; then
+    echo "${RED}Slave crashed; failing test${NORMAL}"
+    exit 2
+  fi
+
+  atexit kill ${AGENT_PID}
+}
+
+setup_env
+start_master
+start_agent
 
 # The main event!
 ${MULTIROLE_FRAMEWORK} \
@@ -153,19 +144,3 @@ ${MULTIROLE_FRAMEWORK} \
         ]
       }'
 STATUS=${?}
-
-# # Make sure the disk full framework "failed".
-# if [[ ! ${STATUS} -eq 1 ]]; then
-#   echo "${RED} Disk full framework returned ${STATUS} not 1${NORMAL}"
-#   exit 1
-# fi
-
-# And make sure the agent is still running!
-kill -0 ${AGENT_PID} >/dev/null 2>&1
-STATUS=${?}
-if [[ ${STATUS} -ne 0 ]]; then
-  echo "${RED}Slave crashed; failing test${NORMAL}"
-  exit 2
-fi
-
-exit 0
