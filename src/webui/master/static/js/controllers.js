13139f1d75bb25b86f52c84774afda0f5f5a16e6
Greg Mann <gregorywmann@gmail.com>
2017-03-15 16:10:18 -0700

    Updated the UI to fix maintenance in DC/OS.

--- src/webui/master/static/js/controllers.js
+++ src/webui/master/static/js/controllers.js
@@ -513,7 +513,7 @@
     var update = function() {
       // TODO(haosdent): Send requests to the leading master directly
       // once `leadingMasterURL` is public.
-      $http.jsonp('/master/maintenance/schedule?jsonp=JSON_CALLBACK')
+      $http.jsonp('master/maintenance/schedule?jsonp=JSON_CALLBACK')
       .success(function(response) {
         $scope.maintenance = response;
       })
